db08c8b762348d2cd1167c6d6306ace16d8dbaf8
Chad <chadgit13@gmail.com>
2016-10-05 15:07:27 -0400

    Add fade and pass pin number to demo

--- src/edu/ccsu/cs416/grovedemo/LedDemo.java
+++ src/edu/ccsu/cs416/grovedemo/LedDemo.java
@@ -11,10 +11,19 @@ import com.dexterind.grovepi.sensors.Led;
 public class LedDemo {
     public static void main(String[] args){
         try{
-            Led led = new Led(3);
+            // Default is D3
+            int boardNumber = 3;
+            if (args.length == 1){
+              boardNumber = Integer.parseInt(args[0]);
+            }
+            Led led = new Led(boardNumber);
             led.turnOn();
             Thread.sleep(1000);
             led.turnOff();
+            for (float brightness=0;brightness<=100;brightness+=10){
+                led.setBrightness(brightness);
+                Thread.sleep(100);
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
