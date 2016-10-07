ee57a68ffe744354e917e49dd66c1c4a184004dd
Chad Williams <chadgit13@gmail.com>
2016-10-07 01:40:36 -0400

    Scripts to reflect 416 to 417 name change

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 mkdir -p ./bin
-javac -d ./bin -classpath .:classes:/opt/pi4j/lib/'*':lib/'*' ./src/com/dexterind/grovepi/*.java ./src/com/dexterind/grovepi/sensors/*.java ./src/com/dexterind/grovepi/sensors/base/*.java ./src/com/dexterind/grovepi/events/*.java ./src/com/dexterind/grovepi/utils/*.java ./src/edu/ccsu/cs416/grovedemo/*.java ./test/*.java ./test/edu/ccsu/cs416/*.java ./test/edu/ccsu/cs416/grovedemo/*.java
+javac -d ./bin -classpath .:classes:/opt/pi4j/lib/'*':lib/'*' ./src/com/dexterind/grovepi/*.java ./src/com/dexterind/grovepi/sensors/*.java ./src/com/dexterind/grovepi/sensors/base/*.java ./src/com/dexterind/grovepi/events/*.java ./src/com/dexterind/grovepi/utils/*.java ./src/edu/ccsu/cs417/grovedemo/*.java ./test/*.java ./test/edu/ccsu/cs417/*.java ./test/edu/ccsu/cs417/grovedemo/*.java
