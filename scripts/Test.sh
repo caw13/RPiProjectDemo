79c25c7140303ed259d528cc46173a48c02210eb
Chad Williams <chadgit13@gmail.com>
2016-10-07 01:16:13 -0400

    updated scripts for revised directory structure

--- scripts/Test.sh
+++ scripts/Test.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 # $ JAVA_OPTS='-Dconfig=default' Java/bin/runTest.sh
-cd ./bin; sudo java $JAVA_OPTS -classpath .:classes:/opt/pi4j/lib/'*' edu.ccsu.cs416.grovedemo.LedDemoTest;
+java $JAVA_OPTS -classpath ./bin:/opt/pi4j/lib/'*':./lib/'*' TestSuiteRunner RPiTestSuite;
